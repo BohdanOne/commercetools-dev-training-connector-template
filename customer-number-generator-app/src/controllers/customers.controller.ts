cecdda2dd26ca1ee508db149766eb29f4344a501
BohdanOne <bohdan.imiela@piercegroup.com>
2025-02-19 16:19:06 +0100

    set customer number code

--- customer-number-generator-app/src/controllers/customers.controller.ts
+++ customer-number-generator-app/src/controllers/customers.controller.ts
@@ -9,11 +9,16 @@ import { Resource } from '../interfaces/resource.interface';
  * @param {Resource} resource The resource from the request body
  * @returns {object}
  */
-const create = async (resource: Resource) => {
+const create = async (_resource: Resource) => {
   const updateActions: Array<UpdateAction> = [];
 
-  // TODO: Create the UpdateAction Object to return it to the client
+  const updateAction: UpdateAction = {
+    action: 'setCustomerNumber',
+    // generate a number between 1000 and 1899
+    customerNumber: String(Math.floor(1000 + Math.random() * 900)),
+  };
 
+  updateActions.push(updateAction);
 
   return { statusCode: 201, actions: updateActions };
 };
